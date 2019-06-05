1c3c34bfcc908532fcf4943315e893048509be13
161250106 <437967081@qq.com>
2019-06-05 22:32:05 +0800

    indent fix

--- demo/src/components/DemoPage.vue
+++ demo/src/components/DemoPage.vue
@@ -14,51 +14,51 @@
 </template>
 
 <script>
-  // script区，即JavaScript编写的地方
-  // 这里主要包括在本页面中用到的JS方法和属性
-  // 想要使用外部方法可以在这里import包，例如：
-  export default {
-    name: 'demoPage',
-    // mounted指在页面加载时，一并运行的方法
-    // 比如说你想在页面加载时从后端获取数据，那么就把这些工作都放在mounted的function内执行
-    mounted: function () {
-      this.init()
+// script区，即JavaScript编写的地方
+// 这里主要包括在本页面中用到的JS方法和属性
+// 想要使用外部方法可以在这里import包，例如：
+export default {
+  name: 'demoPage',
+  // mounted指在页面加载时，一并运行的方法
+  // 比如说你想在页面加载时从后端获取数据，那么就把这些工作都放在mounted的function内执行
+  mounted: function () {
+    this.init()
+  },
+  // methods用于写本界面可能用到的方法
+  methods: {
+    init () {
+      // console.log指在控制台输出内容，在浏览器界面按F12打开，选择console即可看到内容了
+      console.log('page named demoPage init')
+      this.hello()
     },
-    // methods用于写本界面可能用到的方法
-    methods: {
-      init () {
-        // console.log指在控制台输出内容，在浏览器界面按F12打开，选择console即可看到内容了
-        console.log('page named demoPage init')
-        this.hello()
-      },
-      consoleShow () {
-        console.log('the butoon is clicked.')
-      },
-      hello () {
-        this.$axios({
-          method: 'get',
-          url: 'http://localhost:8080/hello_world',
-          params: {
-          }
-        }).then(function (res) {
-          console.log(res.data)
-          //this.username = res.data // Now it works correctly
-        }.bind(this)).catch(function (err) {
-          console.log(err)
-        })
-      }
+    consoleShow () {
+      console.log('the butoon is clicked.')
     },
-    // data区存放了本界面需要用到的数据，在HTML/JS中均可用this.value访问到相应属性的值
-    data () {
-      return {
-        name: 'demo',
-        user: {
-          userId: '10000',
-          level: 'level2'
+    hello () {
+      this.$axios({
+        method: 'get',
+        url: 'http://localhost:8080/hello_world',
+        params: {
         }
+      }).then(function (res) {
+        console.log(res.data)
+        // this.username = res.data // Now it works correctly
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }
+  },
+  // data区存放了本界面需要用到的数据，在HTML/JS中均可用this.value访问到相应属性的值
+  data () {
+    return {
+      name: 'demo',
+      user: {
+        userId: '10000',
+        level: 'level2'
       }
     }
   }
+}
 </script>
 
 <style scoped>
